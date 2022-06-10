75c65ff1a02fadae4a3d648ab90d69bd78769bf2
Walter Simon <simonwalter986@gmail.com>
2022-06-10 14:06:57 +0300

    commit tree df362743b64a78d73923af6a306306f73c274ab2

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function catsArray(members) {
+    [cats];
+}
+
+function catsLength(cats) {
+    cats.length;
+}
+
+function destructivelyAppendCat(members) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(members) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(members) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(members) {
+    cats.shift();
+}
+
+function appendCat(members) {
+    const cat = [...cats, "Broom"];
+    return cat;
+}
+
+function prependCat(members) {
+    const cat = ["Arnold", ...cats];
+    return cat;
+}
+
+function removeLastCat(members) {
+    const cat = cats.slice(0, 2);
+    return cat;
+}
+
+function removeFirstCat(members) {
+    const cat = cats.slice(1);
+    return cat;
+}
